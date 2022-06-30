bf3580e43c149b1eb8f6a7d9cf41e8db8bf71db1
Beau J Fortier <beaufortier13@gmail.com>
2022-06-30 15:11:43 -0400

    mostly finished functionality/ need to fix live time updates

--- script.js
+++ script.js
@@ -1,20 +1,157 @@
-var timeDisplay = document.querySelector('.time');
-var dateDisplay =document.querySelector('.date');
-var currentTime = moment().format('hh:mm A');
+var currentTime = moment().format('h:mm A');
 var currentDate = moment().format('dddd, MMMM Do YYYY')
-var hourBtn = document.querySelector('.hour-btn');
-var minBtn = document.querySelector('.min-btn');
-var alarmBtn = document.querySelector('.alarm-btn');
-var timeBtn = document.querySelector('.time-btn');
-var saveBtn = document.querySelector('.save-btn');
-var resetBtn = document.querySelector('.reset-btn');
-var snoozeBtn = document.querySelector('.snooze')
+var alarmHour = 1;
+var alarmHourIndex = 1;
+var alarmMin = 0;
+var amPm = 'AM'
+var alarmTime = localStorage.getItem('alarm');
+var alarmSound = document.querySelector('.alarm-sound');
+var newAlarm;
 
-dateDisplay.textContent = currentDate;
-
-var renderClock = () => {
-    timeDisplay.textContent = currentTime;
+if (alarmTime === null) {
+    alarmTime = alarmHour + ':0' + alarmMin + ' ' + amPm
 }
 
+console.log(alarmTime, currentTime);
+
+var soundAlarm = () => {
+    alarmSound.play();
+}
+
+var pauseAlarm = () => {
+    alarmSound.pause();
+}
+
+$('#switch').on('change', (function () {
+    if (this.checked) {
+        console.log('switch on')
+        if (alarmTime === currentTime) {
+            soundAlarm();
+        };
+    } else {
+        console.log('switch off')
+        pauseAlarm();
+    };
+}));
+
+var renderDate = () => {
+    $('.date').text(currentDate);
+};
+
+var renderClock = () => {
+    $('.time').text(currentTime);
+};
+
+var toggleAlarm = () => {
+        if (alarmMin < 10) {
+            let alarmTime = alarmHour + ':0' + alarmMin + ' ' + amPm;
+            $('.time').text(alarmTime);
+            newAlarm = alarmTime;
+            console.log(newAlarm)
+        };
+
+        if (alarmMin >= 10) {
+            let alarmTime = alarmHour + ':' + alarmMin + ' ' + amPm;
+            $('.time').text(alarmTime);
+            newAlarm = alarmTime;
+        };
+    };
+
+var toggleTime = () => {
+    $('.time').text(currentTime);
+};
+
+var setHour = () => {
+    alarmHour++;
+    alarmHourIndex++;
+
+    console.log(alarmHour);
+    console.log(alarmHourIndex);
+
+    if (alarmHourIndex >= 12) {
+        amPm = 'PM';
+    } else {
+        amPm = 'AM';
+    }
+
+    if (alarmMin < 10) {
+        alarmTime = alarmHour + ':0' + alarmMin + ' ' + amPm;
+        $('.time').text(alarmTime);
+        newAlarm = alarmTime;
+    };
+
+    if (alarmMin >= 10) {
+        alarmTime = alarmHour + ':' + alarmMin + ' ' + amPm;
+        $('.time').text(alarmTime);
+        newAlarm = alarmTime;
+    };
+
+    if (alarmHour > 12) {
+        alarmHour = 1;
+        if (alarmMin < 10) {
+            let alarmTime = alarmHour + ':0' + alarmMin + ' ' + amPm;
+            $('.time').text(alarmTime);
+            newAlarm = alarmTime;
+        };
+
+        if (alarmMin >= 10) {
+            let alarmTime = alarmHour + ':' + alarmMin + ' ' + amPm;
+            $('.time').text(alarmTime);
+            newAlarm = alarmTime;
+        };
+    };
+
+    if (alarmHourIndex > 23) {
+        alarmHourIndex = 1;
+    };
+};
+
+var setMin = () => {
+    alarmMin++
+    console.log(alarmMin)
+    if (alarmMin < 10) {
+        let alarmTime = alarmHour + ':0' + alarmMin + ' ' + amPm;
+        $('.time').text(alarmTime);
+        newAlarm = alarmTime;
+    };
+
+    if (alarmMin >= 10) {
+        let alarmTime = alarmHour + ':' + alarmMin + ' ' + amPm;
+        $('.time').text(alarmTime);
+        newAlarm = alarmTime;
+    };
+
+    if (alarmMin > 59) {
+        alarmMin = 0;
+        let alarmTime = alarmHour + ':' + alarmMin + ' ' + amPm;
+        $('.time').text(alarmTime);
+        newAlarm = alarmTime;
+    };
+};
+
+var saveAlarm = () => {
+    console.log(newAlarm)
+
+    localStorage.setItem('alarm', newAlarm);
+    window.alert('Alarm Saved!');
+};
+
+var resetAlarm = () => {
+    localStorage.clear();
+};
+
+var snoozeAlarm = () => {
+    pauseAlarm();
+    setInterval(soundAlarm, 300000);
+}
+
+renderDate();
 renderClock();
 setInterval(renderClock, 60000);
+
+$('.alarm-btn').on('click', toggleAlarm);
+$('.time-btn').on('click', toggleTime);
+$('.hour-btn').on('click', setHour);
+$('.min-btn').on('click', setMin);
+$('.save-btn').on('click', saveAlarm);
+$('.reset-btn').on('click', resetAlarm);
