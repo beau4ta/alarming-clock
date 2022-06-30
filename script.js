0e1d89bd5b5716565d7074e34610bfe368d67196
Beau J Fortier <beaufortier13@gmail.com>
2022-06-30 11:57:47 -0400

    most css done/ started js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var timeDisplay = document.querySelector('.time');
-var currentTime = moment().format('LTS');
+var dateDisplay =document.querySelector('.date');
+var currentTime = moment().format('hh:mm A');
+var currentDate = moment().format('dddd, MMMM Do YYYY')
+var hourBtn = document.querySelector('.hour-btn');
+var minBtn = document.querySelector('.min-btn');
+var alarmBtn = document.querySelector('.alarm-btn');
+var timeBtn = document.querySelector('.time-btn');
+var saveBtn = document.querySelector('.save-btn');
+var resetBtn = document.querySelector('.reset-btn');
+var snoozeBtn = document.querySelector('.snooze')
 
+dateDisplay.textContent = currentDate;
 
 var renderClock = () => {
     timeDisplay.textContent = currentTime;
 }
 
 renderClock();
-setInterval(renderClock, 1000);
+setInterval(renderClock, 60000);
